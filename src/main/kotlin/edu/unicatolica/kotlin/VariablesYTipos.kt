8f267bc7369bf988b612c1256027b60da300e7b5
Duvhier <duvier.tavera01@unicatolica.edu.co>
2025-08-20 19:57:58 -0500

    Modification My Info

--- src/main/kotlin/edu/unicatolica/kotlin/VariablesYTipos.kt
+++ src/main/kotlin/edu/unicatolica/kotlin/VariablesYTipos.kt
@@ -2,5 +2,13 @@
 package edu.unicatolica.kotlin
 
 fun main() {
-    // variables and data types
+
+    val name: String = "Duvier Tavera"
+    val age: Int= 37
+    val program: String = "Technology Software Development"
+    val interest: String = "Be a Frontend Development"
+
+    val message = "Hello my name is $name, I am $age years old, I'm studying a career in $program and my interest is $interest"
+    println(message)
+
 }
